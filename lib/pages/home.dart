5b74dd828ec577a9cff96d565cbe1e312da15946
Eddie <eddiearnoldz@me.com>
2023-04-02 21:39:15 +0100

    created seconded abstracted widget function for search bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,58 +9,67 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      backgroundColor: Colors.white,
       body: Column(
         children: [
-          Container(
-            margin: const EdgeInsets.only(top:40,left:20,right:20,bottom:40),
-                alignment: Alignment.center,
-            decoration: BoxDecoration(
-              boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.11), 
-                blurRadius: 40,
-                spreadRadius: 0.0)
-        ]),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: EdgeInsets.all(15),
-                hintText: 'Search Pancake',
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SvgPicture.asset('assets/icons/search_icon.svg'),
-                ),
-                suffixIcon: Container(
-                  width: 100,
-                  child: IntrinsicHeight(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        const VerticalDivider(
-                          color: Colors.black,
-                          indent: 10,
-                          endIndent: 10,
-                          thickness: 0.2,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SvgPicture.asset('assets/icons/filter_icon.svg',
-                          height: 30,
-                          width: 30,),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  borderSide: BorderSide.none)
-              ),
-            ),
-          )
+          _searchField()
       ]),
     );
+  }
+
+  Container _searchField() {
+    return Container(
+          margin: const EdgeInsets.only(top:40,left:20,right:20,bottom:40),
+              alignment: Alignment.center,
+          decoration: BoxDecoration(
+            boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.11), 
+              blurRadius: 40,
+              spreadRadius: 0.0)
+      ]),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: EdgeInsets.all(15),
+              hintText: 'Search Pancake',
+              hintStyle: const TextStyle(
+                color: Colors.redAccent,
+                fontSize: 16
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset('assets/icons/search_icon.svg'),
+              ),
+              suffixIcon: Container(
+                width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.2,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SvgPicture.asset('assets/icons/filter_icon.svg',
+                        height: 30,
+                        width: 30,),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none)
+            ),
+          ),
+        );
   }
 
   AppBar appBar() {
